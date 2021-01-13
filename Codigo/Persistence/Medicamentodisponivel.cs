d2f4d3803d17985b54fb94e7a180c7af38883edb
faelteles <rafael.teles80@hotmail.com>
2021-01-13 11:54:27 -0300

    Criação e atualização da classe persistencia seguindo o projeto de banco de dados do workbench resolve #88

---
+++ Codigo/Persistence/Medicamentodisponivel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Persistence
+{
+    public partial class Medicamentodisponivel
+    {
+        public Medicamentodisponivel()
+        {
+            Solicitacaomedicamento = new HashSet<Solicitacaomedicamento>();
+        }
+
+        public int IdDisponibilizacaoMedicamento { get; set; }
+        public int IdMedicamento { get; set; }
+        public int IdFarmacia { get; set; }
+        public DateTime DataInicioDisponibilizacao { get; set; }
+        public DateTime DataFimDisponibilizacao { get; set; }
+        public int QuantidadeDisponibilizacao { get; set; }
+        public string Lote { get; set; }
+        public string Quantidade { get; set; }
+        public DateTime ValidadeMes { get; set; }
+        public string ValidadeAno { get; set; }
+        public string StatusMedicamento { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public int QuantidadeReservada { get; set; }
+        public int QuantidadeEntregue { get; set; }
+        public int QuantidadeDisponivel { get; set; }
+        public byte[] Imagem { get; set; }
+
+        public virtual Farmacia IdFarmaciaNavigation { get; set; }
+        public virtual Medicamento IdMedicamentoNavigation { get; set; }
+        public virtual ICollection<Solicitacaomedicamento> Solicitacaomedicamento { get; set; }
+    }
+}
---
+++ Codigo/Persistence/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Persistence
+{
+    public partial class Usuario
+    {
+        public Usuario()
+        {
+            Solicitacaomedicamento = new HashSet<Solicitacaomedicamento>();
+        }
+
+        public int IdUsuario { get; set; }
+        public int IdFarmacia { get; set; }
+        public string TipoUsuario { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+        public string Sexo { get; set; }
+        public string Logradouro { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+        public virtual Farmacia IdFarmaciaNavigation { get; set; }
+        public virtual ICollection<Solicitacaomedicamento> Solicitacaomedicamento { get; set; }
+    }
+}
