5c30a2e8be49465f24a5d778e03627cb4d59a246
Henrique Prado <henrique.mxnero@gmail.com>
2021-01-14 10:02:19 -0300

    Implementação do caso de uso Manter Medicamento close #92

---
+++ Codigo/IdrugWeb/Models/MedicamentoModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdrugWeb.Models
+{
+    public class MedicamentoModel
+    {
+        [Display(Name = "Código")]
+        [Key]
+        [Required(ErrorMessage = "Código do medicamento é obrigatório")]
+        public int IdMedicamento { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(45, MinimumLength = 5, ErrorMessage = "Nome do medicamento deve ter entre 5 e 45 caracteres")]
+        public string Nome { get; set; }
+
+
+        public string CodigoBarras { get; set; }
+
+        public string Fabricante { get; set; }
+
+
+
+    }
+}
