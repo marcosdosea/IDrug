c4266c5520a86b0df8a3f1fd0901670d6bb84c42
Henrique Prado <henrique.mxnero@gmail.com>
2021-01-23 13:58:07 -0300

    Correção do método Create do caso de uso Manter Medicamento resolve #110

--- Codigo/IdrugWeb/Models/MedicamentoModel.cs
+++ Codigo/IdrugWeb/Models/MedicamentoModel.cs
@@ -13,12 +13,18 @@ namespace IdrugWeb.Models
         [Required(ErrorMessage = "Código do medicamento é obrigatório")]
         public int IdMedicamento { get; set; }
 
+
         [Required(ErrorMessage = "Campo requerido")]
         [StringLength(45, MinimumLength = 5, ErrorMessage = "Nome do medicamento deve ter entre 5 e 45 caracteres")]
         public string Nome { get; set; }
 
-
         public string CodigoBarras { get; set; }
+
+
+        [Display(Name = "Categoria Medicamento")]
+        public int IdCategoriaMedicamento { get; set; }
+
+        
 
         public string Fabricante { get; set; }
 
