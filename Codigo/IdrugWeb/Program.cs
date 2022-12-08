6c77e790a616fb8ffec42582240611981a60b8ce
Joanne PC casa <joannestephany@gmail.com>
2022-12-08 00:07:00 -0300

    ADD: Migrando iDrugWeb
    Arquivos do iDrugWeb agora em .NET 6

--- Codigo/IdrugWeb/Program.cs
+++ Codigo/IdrugWeb/Program.cs
@@ -1,26 +1,90 @@
+using Core;
+using Core.Service;
+using Core.Services;
+using IdrugWeb.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Service;
+using Usuario = IdrugWeb.Areas.Identity.Data.Usuario;
 
-namespace IdrugWeb
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("IdrugConnection") ?? throw new InvalidOperationException("Connection string 'IdrugConnection' not found.");
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DBContext>(
+                options => options.UseMySQL(connectionString));
+builder.Services.AddDbContext<IdentityContext>(
+                options => options.UseMySQL(connectionString));
+
+builder.Services.AddDefaultIdentity<Usuario>(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    // SignIn settings
+    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    // Password settings
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+
+    // Default User settings.
+    options.User.AllowedUserNameCharacters =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = false;
+
+    // Default Lockout settings
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+}).AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<IdentityContext>();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+    options.Cookie.Name = "YourAppCookieName";
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+    options.LoginPath = "/Identity/Account/Login";
+
+    options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+    options.SlidingExpiration = true;
+});
+
+// injecao dependencia Services
+builder.Services.AddTransient<IMedicamentoService, MedicamentoService>();
+builder.Services.AddTransient<ICategoriaMedicamentoService, CategoriaMedicamentoService>();
+builder.Services.AddTransient<IFarmaciaService, FarmaciaService>();
+builder.Services.AddTransient<IDisponibilizarMedicamentoService, DisponibilizarMedicamentoService>();
+builder.Services.AddTransient<IUsuarioService, UsuarioService>();
+
+// injecao dependencia mappers
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapRazorPages();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
